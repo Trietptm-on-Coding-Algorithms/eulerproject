9459c58342359cbe3ebfe9c548d88ff648e05c25
kennel209 <kennel209@gmail.com>
2013-04-02 21:42:08 +0800

    243 done

--- 243.py
+++ 243.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+from eulerutils import genprime
+
 def totient_sieve(L):
     ''' return list of totient(i) for i from 0 to n-1.
  
@@ -11,8 +13,6 @@ def totient_sieve(L):
         if i == t[i]:
             for j in xrange(i,L+1,i): t[j]-=t[j]/i
     return t
-
-print 'inited'
 
 def findR(a,b,start=2):
     tots = totient_sieve(b**2)
@@ -26,3 +26,29 @@ def findR(a,b,start=2):
        
 #print findR(15499,94744,5000000)
 #failed 10**8 out of mem
+
+def findR2(a,b):
+    # phi = pi(n*(1-1/p))
+    gp = genprime()
+    r,l = b,a
+    d = 1
+    while True:
+        p = gp.next()
+        rn = r*(p-1)
+        ln = l*p
+        dn = d*p
+        # find the most 2*3*5*...value
+        if rn*dn<ln*(dn-1):
+            #test small values
+            for i in xrange(2,p):
+                if r*d*i < l*(d*i-1):
+                    return d*i
+        r = rn
+        l = ln
+        d = dn
+
+    return 0
+
+print findR2(4,10)
+print findR2(15499,94744)
+
